c2ccc2e08539615f945ebfe11c502e8273dac3d3
Uriel Avalos <uri@frankandrobot.com>
2012-07-31 21:58:42 -0400

    transfer

--- src/com/frankandrobot/reminderer/Database/DatabaseInterface.java
+++ src/com/frankandrobot/reminderer/Database/DatabaseInterface.java
@@ -4,6 +4,7 @@ import java.util.Calendar;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.content.ContentProviderResult;
 import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.ContentValues;
@@ -11,9 +12,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
 import android.os.Parcel;
 import android.util.Log;
 
+import com.android.calendar.AsyncQueryService.Operation;
+import com.android.calendar.AsyncQueryServiceHelper.OperationInfo;
 import com.frankandrobot.reminderer.Alarm.AlarmConstants;
 import com.frankandrobot.reminderer.Helpers.Logger;
 import com.frankandrobot.reminderer.Parser.Task;
@@ -30,7 +35,14 @@ import android.support.v4.content.Loader;
 public class DatabaseInterface {
     static private String TAG = "R:DbInterface";
 
-    public static void addTask(Context context, Task task) {
+    private static ContentValues addToContentValues(Task task) {
+	ContentValues values = new ContentValues();
+	values.put(DbColumns.TASK_DUE_DATE, task.getDateTimeForDb());
+	values.put(DbColumns.TASK, task.getTaskForDb());
+	return values;
+    }
+    
+    public static void addTask(Context context, Handler handler, Task task) {
 	if (Logger.LOGV) {
 	    Log.v(TAG, "Saving task:\n" + task.toString());
 	    Log.v(TAG,
@@ -39,10 +51,10 @@ public class DatabaseInterface {
 	}
 	ContentResolver resolver = context.getContentResolver();
 	// create content values from Task object
-	ContentValues values = new ContentValues();
-	values.put(DbColumns.TASK_DUE_DATE, task.getDateTimeForDb());
-	values.put(DbColumns.TASK, task.getTaskForDb());
+	ContentValues values = addToContentValues(task);
 	// add content values to db
+	Intent intent = new Intent(AsyncHelper.QUERY_HELPER);
+	intent.
 	Uri uri = resolver.insert(DbColumns.CONTENT_URI, values);
 	// get id
 	String segment = uri.getPathSegments().get(1);
@@ -60,7 +72,7 @@ public class DatabaseInterface {
 	// TODO rewrite using Cursor groups
 	// TODO make this asynchronous
 
-	//disable the old alarm if any
+	// disable the old alarm if any
 	disableAlert(context);
 	// get all tasks due after curTime
 	long curTime = System.currentTimeMillis();
@@ -198,5 +210,52 @@ public class DatabaseInterface {
 		new Intent(AlarmConstants.TASK_ALARM_ALERT),
 		PendingIntent.FLAG_CANCEL_CURRENT);
 	am.cancel(sender);
+    }
+
+    /*
+     * /////////////////////////////////////////////////////////////////////////
+     */
+
+    class AsyncHelper extends Handler {
+
+	static public String QUERY_HELPER = "com.frankandrobot.reminderer.query";
+	static public String QUERY_HANDLER= "handler";
+	
+	@Override
+	public void handleMessage(Message msg) {
+	    OperationInfo info = (OperationInfo) msg.obj;
+
+	    int token = msg.what;
+	    int op = msg.arg1;
+
+	    if (localLOGV) {
+		Log.d(TAG, "AsyncQueryService.handleMessage: token=" + token
+			+ ", op=" + op + ", result=" + info.result);
+	    }
+
+	    // pass token back to caller on each callback.
+	    switch (op) {
+	    case Operation.EVENT_ARG_QUERY:
+		onQueryComplete(token, info.cookie, (Cursor) info.result);
+		break;
+
+	    case Operation.EVENT_ARG_INSERT:
+		onInsertComplete(token, info.cookie, (Uri) info.result);
+		break;
+
+	    case Operation.EVENT_ARG_UPDATE:
+		onUpdateComplete(token, info.cookie, (Integer) info.result);
+		break;
+
+	    case Operation.EVENT_ARG_DELETE:
+		onDeleteComplete(token, info.cookie, (Integer) info.result);
+		break;
+
+	    case Operation.EVENT_ARG_BATCH:
+		onBatchComplete(token, info.cookie,
+			(ContentProviderResult[]) info.result);
+		break;
+	    }
+	}
     }
 }
