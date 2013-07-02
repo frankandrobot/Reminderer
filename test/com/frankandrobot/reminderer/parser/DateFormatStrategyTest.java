f67fddbe0a8f653bf07be12852d84da71606498c
uriel.avalos <uriel.avalos@teachhealth.com>
2013-07-02 13:10:54 -0400

    [refactoring] fixed package name that should have been fixed

---
+++ test/com/frankandrobot/reminderer/parser/DateFormatStrategyTest.java
@@ -0,0 +1,25 @@
+package com.frankandrobot.reminderer.parser;
+
+import org.junit.Test;
+
+/**
+ * Created by Uriel.Avalos on 6/27/13.
+ */
+public class DateFormatStrategyTest
+{
+    /**
+     * Test long format
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testBruteForceFind1() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testParse() throws Exception {
+
+    }
+}
