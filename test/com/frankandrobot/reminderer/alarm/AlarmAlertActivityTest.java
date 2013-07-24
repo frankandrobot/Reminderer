5c67c658d7795fda5a914d57ce2c2ef3997d42ee
uriel avalos <uri@frankandrobot.com>
2013-07-23 21:20:54 -0400

    Added oboelectric for real yo

---
+++ test/com/frankandrobot/reminderer/alarm/AlarmAlertActivityTest.java
@@ -0,0 +1,15 @@
+package com.frankandrobot.reminderer.alarm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+@RunWith(RobolectricTestRunner.class)
+public class AlarmAlertActivityTest
+{
+    @Test
+    public void testOnCreateLoader() throws Exception
+    {
+
+    }
+}
